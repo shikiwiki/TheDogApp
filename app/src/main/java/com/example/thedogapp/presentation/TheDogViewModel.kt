2c8b9400dab0aea3aa3ce48ef793ba32064c6330
ViktoriaAstashonok <122493150+ViktoriaAstashonok@users.noreply.github.com>
2024-01-23 14:31:23 +0300

    fixes

--- app/src/main/java/com/example/thedogapp/presentation/TheDogViewModel.kt
+++ app/src/main/java/com/example/thedogapp/presentation/TheDogViewModel.kt
@@ -3,12 +3,9 @@ package com.example.thedogapp.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.thedogapp.domain.repository.DogListRepository
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@OptIn(DelicateCoroutinesApi::class)
 class TheDogViewModel @Inject constructor(private val dogListRepository: DogListRepository) :
     ViewModel() {
     init {
